ca82154b8f1ed557bc195210a22a999f6a103929
joelwalice <joelwalice@gmail.com>
2025-05-07 17:32:26 +0530

    Added Day-3 Content

--- May 7, 2025/JOINS.sql
+++ May 7, 2025/JOINS.sql
@@ -57,3 +57,44 @@ begin
 end
 
 proc_UpdateProductSpec 1, '24GB';
+
+create table Posts
+(id int primary key,
+title nvarchar(100),
+user_id int,
+body nvarchar(max))
+Go
+
+select * from Posts
+
+create proc proc_BulInsertPosts(@jsondata nvarchar(max))
+as
+begin
+	insert into Posts(user_id,id,title,body)
+	select userId,id,title,body from openjson(@jsondata)
+	with (userId int,id int, title varchar(100), body varchar(max))
+end
+  
+
+proc_BulInsertPosts '
+[
+  {
+    "userId": 1,
+    "id": 1,
+    "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+    "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+  },
+  {
+    "userId": 1,
+    "id": 2,
+    "title": "qui est esse",
+    "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+  }]'
+
+ create procedure proc_BringPosts(@pid int)
+ as
+ begin
+	SELECT * FROM Posts Where id = @pid;
+ end
+
+ proc_BringPosts 1
