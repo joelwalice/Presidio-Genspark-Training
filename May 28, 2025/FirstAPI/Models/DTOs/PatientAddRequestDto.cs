d110103906a2ae48e1141a608152169d706bdf4c
Presidio <joelwalice@gmail.com>
2025-05-28 22:54:45 +0530

    Added Day-18 Contents

--- May 28, 2025/FirstAPI/Models/DTOs/PatientAddRequestDto.cs
+++ May 28, 2025/FirstAPI/Models/DTOs/PatientAddRequestDto.cs
@@ -11,7 +11,7 @@ namespace FirstAPI.Models.DTOs.PatientAddRequestDto
         public string Name { get; set; } = string.Empty;
         public int Age { get; set; }
         public string Email { get; set; } = string.Empty;
-        public string PhoneNumber { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
         public List<AppointmentAddRequestDto>? Appointments { get; set; } = new List<AppointmentAddRequestDto>();
     }
 }
