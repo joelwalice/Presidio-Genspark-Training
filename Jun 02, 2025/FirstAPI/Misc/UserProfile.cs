e4734be6f2eef6913896568d06ecb776019404bf
JoelWalice <joelwalice@gmail.com>
2025-06-03 10:47:08 +0530

    Added Day-21 Contents

---
+++ Jun 02, 2025/FirstAPI/Misc/UserProfile.cs
@@ -0,0 +1,19 @@
+using AutoMapper;
+using FirstAPI.Models;
+using FirstAPI.Models.DTOs.DoctorSpecialities;
+
+namespace FirstAPI.Misc
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<DoctorAddRequestDto, User>()
+            .ForMember(dest => dest.Username, act => act.MapFrom(src => src.Email));
+           
+            CreateMap< User,DoctorAddRequestDto>()
+            .ForMember(dest => dest.Email, act => act.MapFrom(src => src.Username));
+   
+        }
+    }
+}
