4a08f5709ec04db166e6fcf811af2868b6cdd0af
JoelWalice <joelwalice@gmail.com>
2025-06-03 10:47:08 +0530

    Added Day-21 Contents

---
+++ Jun 02, 2025/FirstAPI/Interfaces/IAuthenticationServices.cs
@@ -0,0 +1,9 @@
+using FirstAPI.Models.DTOs.DoctorSpecialities;
+
+namespace FirstAPI.Interfaces
+{
+    public interface IAuthenticationService
+    {
+        public Task<UserLoginResponse> Login(UserLoginRequest user);
+    }
+}
