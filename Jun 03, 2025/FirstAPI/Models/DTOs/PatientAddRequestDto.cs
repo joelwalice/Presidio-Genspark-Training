83ea81d8339d093408770b3f1f2756e7378cc0bd
JoelWalice <joelwalice@gmail.com>
2025-06-03 18:13:13 +0530

    Added Day-22 Contents

--- Jun 03, 2025/FirstAPI/Models/DTOs/PatientAddRequestDto.cs
+++ Jun 03, 2025/FirstAPI/Models/DTOs/PatientAddRequestDto.cs
@@ -12,6 +12,7 @@ namespace FirstAPI.Models.DTOs.PatientAddRequestDto
         public int Age { get; set; }
         public string Email { get; set; } = string.Empty;
         public string Phone { get; set; } = string.Empty;
-        public List<AppointmentAddRequestDto>? Appointments { get; set; } = new List<AppointmentAddRequestDto>();
+        public string Password { get; set; } = string.Empty;
+        public ICollection<AppointmentAddRequestDto>? Appointments { get; set; } = new List<AppointmentAddRequestDto>();
     }
 }
--- Jun 03, 2025/FirstAPI/Services/PatientService.cs
+++ Jun 03, 2025/FirstAPI/Services/PatientService.cs
@@ -3,7 +3,8 @@ using FirstAPI.Interfaces;
 using System.Threading.Tasks;
 using FirstAPI.Models.DTOs.PatientAddRequestDto;
 using FirstAPI.Repositories;
-
+using FirstAPI.Services;
+using AutoMapper;
 
 namespace FirstAPI.Services
 {
@@ -11,36 +12,65 @@ namespace FirstAPI.Services
     {
         public IRepository<int, Patient> _patientRepository;
         public IRepository<int, Appointment> _appointmentRepository;
+        private readonly IRepository<string, User> _userRepository;
+        private readonly IMapper _mapper;
+        private readonly IEncryptionService _encryptionService;
         public PatientService(IRepository<int, Patient> PatientRepository,
-                              IRepository<int, Appointment> appointmentRepository)
+                              IRepository<int, Appointment> appointmentRepository,
+                              IRepository<string, User> userRepository,
+                              IMapper mapper,
+                              IEncryptionService encryptionService)
         {
             _patientRepository = PatientRepository;
             _appointmentRepository = appointmentRepository;
+            _userRepository = userRepository;
+            _mapper = mapper;
+            _encryptionService = encryptionService;
         }
 
         public async Task<Patient> AddPatient(PatientAddRequestDto patientDto)
         {
-            var newPatient = new Patient
+            try
             {
-                Name = patientDto.Name,
-                Age = patientDto.Age,
-                Email = patientDto.Email,
-                Phone = patientDto.Phone,
-            };
-            var createdPatient = await _patientRepository.Add(newPatient);
-            if (patientDto.Appointments != null) {
-                foreach (var appointment in patientDto.Appointments)
+                var user = _mapper.Map<PatientAddRequestDto, User>(patientDto);
+                var encryptedData = await _encryptionService.EncryptData(new EncryptModel
                 {
-                    var newAppointment = new Appointment
+                    Data = patientDto.Password 
+                });
+                user.Password = encryptedData.EncryptedData;
+                user.HashKey = encryptedData.HashKey;
+                user.Role = "Patient";
+                user = await _userRepository.Add(user);
+
+                var newPatient = new Patient
+                {
+                    Name = patientDto.Name,
+                    Age = patientDto.Age,
+                    Email = patientDto.Email,
+                    Phone = patientDto.Phone,
+                    User = user 
+                };
+                var createdPatient = await _patientRepository.Add(newPatient);
+
+                if (patientDto.Appointments != null)
+                {
+                    foreach (var appointment in patientDto.Appointments)
                     {
-                        AppointmentDateTime = appointment.AppointmentDateTime,
-                        DoctorId = appointment.DoctorId,
-                        PatientId = createdPatient.Id
-                    };
-                    await _appointmentRepository.Add(newAppointment);
+                        var newAppointment = new Appointment
+                        {
+                            AppointmentDateTime = appointment.AppointmentDateTime,
+                            DoctorId = appointment.DoctorId,
+                            PatientId = createdPatient.Id
+                        };
+                        await _appointmentRepository.Add(newAppointment);
+                    }
                 }
+                return createdPatient;
             }
-            return createdPatient;
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
         }
 
     }
