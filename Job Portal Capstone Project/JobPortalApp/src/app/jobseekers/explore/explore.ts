39e832899080ca63fa3f345695e01cbbd0fcb843
Presidio <joelwalice@gmail.com>
2025-07-11 18:16:59 +0530

    Added modifications to the Capstone projects

--- Job Portal Capstone Project/JobPortalApp/src/app/jobseekers/explore/explore.ts
+++ Job Portal Capstone Project/JobPortalApp/src/app/jobseekers/explore/explore.ts
@@ -12,36 +12,46 @@ import { JobSeekerService } from '../../services/user/job-seeker';
 })
 export class Explore implements OnInit {
   searchTerm: string = "";
+  allJobs: any[] = [];
   filteredJobs: any[] = [];
-  constructor(private JobSeekerService: JobSeekerService, private router: Router) { }
+
+  constructor(private JobSeekerService: JobSeekerService, private router: Router) {}
+
   ngOnInit(): void {
     const token = localStorage.getItem("JwtToken");
+
     const getJobDetails = () => {
       if (token) {
         this.JobSeekerService.getJobDetails().subscribe({
           next: (data) => {
-            console.log(data)
+            this.allJobs = data;
             this.filteredJobs = data;
           },
           error: (err) => {
             console.log(err);
           }
-        })
-      }
-      else {
+        });
+      } else {
         setTimeout(getJobDetails, 100);
       }
-    }
+    };
+
     getJobDetails();
   }
-  onSearch() {
 
+  onSearch() {
+    const term = this.searchTerm.toLowerCase();
+    this.filteredJobs = this.allJobs.filter(job =>
+      job.title.toLowerCase().includes(term) ||
+      job.companyName.toLowerCase().includes(term) ||
+      job.requirements.toLowerCase().includes(term)
+    );
   }
+
   borderColors = ['border-blue-500', 'border-green-500', 'border-yellow-400', 'border-purple-500'];
 
   getBorderClass(title: string): string {
     const index = title.length % this.borderColors.length;
     return this.borderColors[index];
   }
-
 }
